edcdff94308bae73286c770cdee51498d753d937
clark chen <chenhuajie1@gmail.com>
2016-11-10 16:00:52 -0500

    change db to postgresql

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20161110031539) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "records", force: :cascade do |t|
     t.string   "title"
     t.date     "date"
